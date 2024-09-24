199c09790d9e2c53e7a45da266e1af6bc098c410
syedBahadarKhan <ba4646465@gmail.com>
2024-09-24 17:07:26 +0500

    applied some changes using css

---
+++ index.js
@@ -0,0 +1,22 @@
+function addTask(){
+    var taskInput = document.getElementById("taskinput");
+    var taskList = document.getElementById("tasklist");
+
+    //here we create list item for unorder list
+    var li = document.createElement("li");
+    li.textContent = taskInput.Value;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
